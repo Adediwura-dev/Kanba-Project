f4072f29b35b1ee7e64ae902b55a0154d1ae935e
jubrilmunirat <jubrilyetunde5@gmail.com>
2026-09-22 18:46:56 +0100

    #modified: src/Statics/Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./Statics/Header";
 import KanbaBoard from "./Pages/KanbaBoard";
+
 
 function App() {
 	return (
 		<>
+			<Header />
 			<KanbaBoard />
 		</>
 	);
---
+++ src/Statics/Header.jsx
@@ -0,0 +1,31 @@
+
+
+import logo from "../assets/logo.png"
+import { Trash, Menu } from "lucide-react"
+
+const Header = () => {
+    return (
+        <div className="flex justify-between items-center py-10 px-10 font-bold text-xl items-center top-0 left-0 z-50  ">
+            <div className="flex justify-between gap-6 items-center cursor-pointer ">
+                <Menu className="font-bold text-xl hidden max-md:block h-10 w-10" />
+                <div className="">
+                    <img src={logo} className="max-w-[150px] min-h-[60px]" />
+                </div>
+                <div className="flex gap-6 items-center cursor-pointer max-md:hidden ">
+                    <nav className="bg-white-500 hover:bg-blue-300 text-black font-bold py-2 px-4 rounded">Home</nav>
+                    <nav className="bg-white-500 hover:bg-blue-300 text-black font-bold py-2 px-4 rounded">About Us</nav>
+                    <nav className="bg-white-500 hover:bg-blue-300 text-black font-bold py-2 px-4 rounded"> Kanba Board</nav>
+                </div>
+            </div>
+            <div className="flex justify-between gap-6 cursor-pointer">
+                <Trash className="bg-white-500 hover:bg-blue-300 text-black font-bold py-2 px-4 rounded " />
+                <div className="flex gap-5 cursor-pointer max-md:hidden">
+                    <button className="cursor-pointerbg-white-500 hover:bg-blue-300 text-black font-bold py-2 px-4 rounded">Sign Up</button> 
+                    <button className="cursor-pointerbg-white-500 hover:bg-blue-300 text-black font-bold py-2 px-4 rounded">Log in</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
