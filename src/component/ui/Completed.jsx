3828e4e80706d5fb6fc0d0e61671524dcce399d2
Bushra <bushragbadegesin@gmail.com>
2026-09-24 23:56:26 +0100

    add scroll to kanban columns

--- src/component/ui/Completed.jsx
+++ src/component/ui/Completed.jsx
@@ -16,7 +16,7 @@ export default function Completed({ cards, deleteCard }) {
   return (
     <div
       ref={setNodeRef}
-      className="flex flex-col gap-4 justify-start py-3 px-4 rounded-3xl bg-[#FFD1DC]"
+      className="flex flex-col max-h-[calc(100vh-200px)] overflow-y-auto custom-scrollbar gap-4 justify-start py-3 px-4 rounded-3xl bg-[#FFD1DC]"
     >
       <div>
         <div className="flex gap-2 bg-[#FBB1C3] w-fit py-1 px-2 text-sm rounded-2xl items-center">
